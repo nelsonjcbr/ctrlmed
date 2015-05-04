bcc605c917011eb40e3defc44e142da56729242d
Nelson Jose Coelho <nelsonjcbr@gmail.com>
2015-05-03 22:28:37 -0300

    AJustes layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Med::Application.routes.draw do
+  get "welcome/index"
   resources :solexames
 
   resources :clilabs
@@ -24,7 +25,7 @@ Med::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pacientes#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
